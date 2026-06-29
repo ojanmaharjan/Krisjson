727ddf3c91bf3136a9e1477c16afbc958ea0c65c
ojanmaharjan <ojanmaharjan03@gmail.com>
2026-06-29 18:05:01 +0545

    new application

---
+++ src/main/java/com/infodev/krisjson/login/user/User.java
@@ -0,0 +1,37 @@
+package com.infodev.krisjson.login.user;
+
+import com.infodev.krisjson.login.user.role.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * author: Ojan Maharjan
+ * createdDate: 6/29/26
+ * projectName: branchless-banking
+ **/
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false , unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
